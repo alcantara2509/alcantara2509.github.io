1a589588d290dde6f9ad47ebe4fdad208a4fa1d6
alcantara2509 <alcantara2509@gmail.com>
2020-03-04 15:54:24 -0300

    botoes

--- scriptTD.js
+++ scriptTD.js
@@ -3,14 +3,12 @@ let btnCriar = document.querySelector('#criar-tarefa');
 let texto = document.querySelector('#texto-tarefa');
 let btnApagaTudo = document.querySelector('#apaga-tudo');
 let btnRemoverFinalizados = document.querySelector('#remover-finalizados');
+let btnRemoverSelecionados = document.querySelector('#remover-selecionado');
+let btnMoverCima = document.querySelector('#mover-cima');
 
 btnCriar.addEventListener("click", addLista);
 
 function addLista(){
-  if (texto.value == 0){
-    texto.style.borderColor = "#fb7f7f"
-  } else {
-    texto.style.borderColor = "initial";
 
     let novoItem = document.createElement('li')
     novoItem.innerText = texto.value
@@ -41,6 +39,22 @@ function addLista(){
     }
     apagarFinalizados();
 
+    function apagarSelecionados() {
+      btnRemoverSelecionados.addEventListener("click", function() {
+        let selected = document.querySelector(".selected");
+        lista.removeChild(selected)
+      })
+    }
+    apagarSelecionados();
+
+    function moverCima() {
+      btnMoverCima.addEventListener("click", function() {let sp2 = document.querySelector(".selected");
+      let elPai = sp2.parentNode
+      elPai.insertBefore(sp2, novoItem)
+      })
+    }
+    moverCima();
+
     function apagarTudo(){
       btnApagaTudo.addEventListener("click", function() {
               lista.removeChild(novoItem)
@@ -49,4 +63,3 @@ function addLista(){
       apagarTudo();
 
 };
-}
